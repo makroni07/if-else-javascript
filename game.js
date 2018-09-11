b92b316edb9bb90114727f4936e6495923ead199
M.Abraham <41353138+makroni07@users.noreply.github.com>
2018-09-11 15:46:03 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
-var name="";
+var nama="";
 var peran="";
 
 
-if (name==="") {
+if (nama==="") {
 	console.log("nama tidak boleh kosong");
 }
 
 else if (nama) {
-	console.log("Halo" +nama+ ," selamat datang di proxitya");
+	console.log("Halo" +nama+ ", selamat datang di proxitya");
 }
 
 else if (peran==="") {
@@ -15,18 +15,18 @@ else if (peran==="") {
 }
 
 else if (peran==="Ksatria") {
-	console.log("Ksatria" +nama+ ,"kamu dapat menggunakan pedang untuk menyerang nya");
+	console.log("Ksatria" +nama+ ",kamu dapat menggunakan pedang untuk menyerang nya");
 }
 
 else if (peran==="Tabib") {
-	console.log("Tabib" +nama+ ,"kamu dapat mengobati teman kamu yang terluka");
+	console.log("Tabib" +nama+ ",kamu dapat mengobati teman kamu yang terluka");
 }
 
 
 else if (peran==="Penyihir") {
-	console.log("Penyihir" +nama+ ,"kamu dapat mengunakan sihir untuk membantu teman mu");
+	console.log("Penyihir" +nama+ ",kamu dapat mengunakan sihir untuk membantu teman mu");
 }
 
 else if (nama==="" && peran) {
-	console.log("Harap nama di isi untuk memulai game")
+	console.log("Harap nama di isi untuk memulai game");
 }
