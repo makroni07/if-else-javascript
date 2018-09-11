dbe9f8065faec91fec310dd7b5f2075ca83d225b
Makroni07 <mnaufal4567@gmail.com>
2018-09-11 10:56:49 +0700

    belajar javascript

---
+++ tugas-baru.js
@@ -0,0 +1,65 @@
+// Rumus Kubus
+
+function rumusKubus() {
+ return angka1 * angka1 * angka1
+}
+
+let angka1 = 2;
+
+
+let hasilRumusKubus = rumusKubus(angka1)
+console.log(hasilRumusKubus);
+
+// Rumus Jajar Genjang
+
+function rumusJajargenjang() {
+ return angka4 * angka5
+}
+
+let angka4 = 2;
+let angka5 = 5;
+
+
+let hasilRumusJajargenjang = rumusJajargenjang(angka4,angka5)
+console.log(hasilRumusJajargenjang);
+
+// Rumus Luas Segitiga
+
+function rumusLuassegitiga() {
+ return 0.5 * angka6 * angka7
+}
+
+let angka6 = 2;
+let angka7 = 8;
+
+
+let hasilRumusLuassegitiga = rumusLuassegitiga(angka6,angka7)
+console.log(hasilRumusLuassegitiga);
+
+// Rumus Tabung
+
+function rumusTabung() {
+ return 22 / 7 * angka8 * angka9 * angka10
+}
+
+let angka8 = 2;
+let angka9 = 8;
+let angka10 = 7;
+
+
+let hasilRumusTabung= rumusTabung(angka8,angka9,angka10)
+console.log(hasilRumusTabung);
+
+
+// rumus luas persegi
+
+function rumusPersegi() {
+ return angka11 * angka12 
+}
+
+let angka11 = 9;
+let angka12 = 9;
+
+
+let hasilRumusPersegi= rumusPersegi(angka11,angka12)
+console.log(hasilRumusPersegi);
