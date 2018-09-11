711e24e3afd24f3e2db824d8dc2a211e88fdd9cd
Makroni07 <mnaufal4567@gmail.com>
2018-09-11 15:41:16 +0700

    Game-Sederhana

---
+++ game.js
@@ -0,0 +1,32 @@
+var name="";
+var peran="";
+
+
+if (name==="") {
+	console.log("nama tidak boleh kosong");
+}
+
+else if (nama) {
+	console.log("Halo" +nama+ ," selamat datang di proxitya");
+}
+
+else if (peran==="") {
+	console.log("silahkan pilih peran untuk memulaik game");
+}
+
+else if (peran==="Ksatria") {
+	console.log("Ksatria" +nama+ ,"kamu dapat menggunakan pedang untuk menyerang nya");
+}
+
+else if (peran==="Tabib") {
+	console.log("Tabib" +nama+ ,"kamu dapat mengobati teman kamu yang terluka");
+}
+
+
+else if (peran==="Penyihir") {
+	console.log("Penyihir" +nama+ ,"kamu dapat mengunakan sihir untuk membantu teman mu");
+}
+
+else if (nama==="" && peran) {
+	console.log("Harap nama di isi untuk memulai game")
+}
